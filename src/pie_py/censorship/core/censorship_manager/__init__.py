5085051bd86d5a8d0b378da654c97b568cd8b7b7
ityeri <ityeri9@gmail.com>
2026-07-26 19:57:32 +0900

    rm: pie_py/censorship package

--- src/pie_py/censorship/core/censorship_manager/__init__.py
+++
@@ -1,9 +0,0 @@
-from .censorship_manager import CensorshipManager
-from .censorship_policy import CensorshipPolicy
-from . import exceptions
-
-__all__ = [
-    'CensorshipManager',
-    'CensorshipPolicy',
-    'exceptions'
-]
--- src/pie_py/censorship/core/text_converter/__init__.py
+++
@@ -1,9 +0,0 @@
-from .text_converter import TextConverter
-from . import convert_funcs
-from . import utils
-
-__all__ = [
-    'TextConverter',
-    'convert_funcs',
-    'utils'
-]
