b0e36eaeac313dd12ac51547e18e7311c3e061f6
ityeri <ityeri9@gmail.com>
2026-07-26 20:04:19 +0900

    rm: Eliminate name of deleted packages from pie_py/extensions.py

--- src/pie_py/extensions.py
+++ src/pie_py/extensions.py
@@ -1,11 +1,8 @@
 preload_modules = [
-    'pie_py.censorship.core.censorship_manager.models'
 ]
 
 extensions = [
     "pie_py.music.music_extension",
-    'pie_py.censorship.censorship_extension',
-    'pie_py.ssudam.ssudam_extension'
 ]
 
 __all__ = [
