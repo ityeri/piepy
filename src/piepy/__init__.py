36fc0e117fbeb49e7a8320853037fb31560e08d9
ityeri <ityeri9@gmail.com>
2026-07-27 22:48:14 +0900

    add: RootContainer class using dependency_injector lib

--- src/piepy/__init__.py
+++ src/piepy/__init__.py
@@ -3,14 +3,17 @@ from piepy import listener
 from piepy import async_listener
 from piepy import utils
 from piepy import music
+
+from piepy.root_container import RootContainer
 from piepy import config
 
 __all__ = [
-    'config',
     'setup',
     'listener',
     'async_listener',
     'utils',
-
     'music',
+
+    'RootContainer',
+    'config'
 ]
