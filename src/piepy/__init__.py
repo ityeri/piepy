d0cb01f954243ac8295b42a50991ae3ede871551
ityeri <ityeri9@gmail.com>
2026-07-27 22:31:11 +0900

    add: Config class, get_config_from_env function

--- src/piepy/__init__.py
+++ src/piepy/__init__.py
@@ -2,14 +2,15 @@ from piepy import setup
 from piepy import listener
 from piepy import async_listener
 from piepy import utils
-
 from piepy import music
+from piepy import config
 
 __all__ = [
-    "setup",
-    "listener",
-    "async_listener",
-    "utils",
+    'config',
+    'setup',
+    'listener',
+    'async_listener',
+    'utils',
 
-    "music",
+    'music',
 ]
