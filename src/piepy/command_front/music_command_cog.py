fa7d19dd45b78eae85b655445c469e2766cad43d
ityeri <ityeri9@gmail.com>
2026-07-28 23:07:38 +0900

    add: MusicCommandCog.rm slash command method

--- src/piepy/command_front/music_command_cog.py
+++ src/piepy/command_front/music_command_cog.py
@@ -4,7 +4,8 @@ from pytubefix import YouTube
 from pytubefix.exceptions import RegexMatchError, VideoUnavailable
 from yspy.__future__ import VideosSearch
 
-from piepy.player_manager import PlayerManager, UrlStreamMusicElement, MusicAddingResult
+from piepy.player_manager import PlayerManager, UrlStreamMusicElement, MusicAddingResult, MusicElement, \
+    MusicRemovingResult
 from piepy.utils import theme
 
 
@@ -21,6 +22,7 @@ class MusicCommandCog(commands.Cog):
         self.player_manager: PlayerManager = player_manager
 
     async def check_user_voice_state(self, ctx: commands.Context, is_first: bool = False) -> bool:
+        # TODO is more better making player status wrapping feature in player manager
         player = self.player_manager.get_player(ctx.guild.id)
 
         if ctx.author.voice is None:
@@ -163,3 +165,54 @@ class MusicCommandCog(commands.Cog):
                 color=theme.OK_COLOR
             )
         )
+
+    class RmFlags(commands.FlagConverter):
+        title_or_index: str | None = \
+            commands.Flag(name="번호나_제목", description="영상의 번호나 제목 또는 제목의 일부를 입력하세요")
+
+    @staticmethod
+    def query_music_naturally(musics: list[MusicElement], title_or_index: str) -> MusicElement | None:
+        try:
+            index: int = int(title_or_index) - 1
+            if 0 <= index: # 맞다 파이썬 음수 인덱스도 있었지
+                try:
+                    return musics[index]
+                except IndexError:
+                    pass
+
+        except ValueError:
+            title: str = title_or_index
+
+            for checking_music in musics:
+                if title in checking_music.title:
+                    return checking_music
+
+        return None
+
+    @commands.hybrid_command(name="빼기", description="재생목록에서 영상을 하나 제거합니다")
+    async def rm(self, ctx: commands.Context, *, flags: RmFlags):
+        is_valid_context = await self.check_user_voice_state(ctx)
+        if not is_valid_context:
+            return
+
+        musics = self.player_manager.get_player(ctx.guild.id).musics
+        target_music = self.query_music_naturally(musics, flags.title_or_index)
+
+        result = self.player_manager.rm_music(ctx.guild.id, music_element=target_music)
+
+        if result == MusicRemovingResult.REMOVED:
+            await ctx.reply(
+                embed=Embed(
+                    title='REMOVED',
+                    description=f'**{target_music.title}** 영상을 재생목록에서 제거했습니다',
+                    color=theme.OK_COLOR
+                )
+            )
+        elif result == MusicRemovingResult.SKIPPED_AND_REMOVED:
+            await ctx.reply(
+                embed=Embed(
+                    title='SKIPPED_AND_REMOVED',
+                    description=f'**{target_music.title}** 영상을 건너뛴 후, 재생목록에서 제거했습니다',
+                    color=theme.OK_COLOR
+                )
+            )
