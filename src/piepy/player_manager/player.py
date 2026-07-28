ac89759c45ccec2cd4542ac21801e9c176b0fb32
ityeri <ityeri9@gmail.com>
2026-07-28 15:58:48 +0900

    add: Missing status changing code in Player.stop method

--- src/piepy/player_manager/player.py
+++ src/piepy/player_manager/player.py
@@ -89,6 +89,8 @@ class Player:
         self.voice_client.stop()
         await self.voice_client.disconnect()
 
+        self.status = PlayerStatus.DONE
+
         await self.on_stop(PlayerStopEvent(self))
 
     def add_last(self, music: MusicElement):
