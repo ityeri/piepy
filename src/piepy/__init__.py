f2cc6032c2c66e7dd90fe47abb5f5e5b1db8922e
ityeri <ityeri9@gmail.com>
2026-07-28 14:22:14 +0900

    add: player_manager and command_front package has added to root __init__.py

--- src/piepy/__init__.py
+++ src/piepy/__init__.py
@@ -2,6 +2,9 @@ from piepy import config
 from piepy.bootstrap import Bootstrapper
 from piepy.root_container import RootContainer
 
+from piepy import player_manager
+from piepy import command_front
+
 
 def main():
     container = RootContainer()
@@ -14,6 +17,9 @@ __all__ = [
     'RootContainer',
     'Bootstrapper',
     'config',
+
+    'player_manager',
+    'command_front',
 
     'main'
 ]
