10583f8bcef7a864728419831feb12158fccc31c
ityeri <ityeri9@gmail.com>
2026-07-28 19:41:52 +0900

    add: MusicRemovingResult is added to player_manager/__init__.py

--- src/piepy/player_manager/__init__.py
+++ src/piepy/player_manager/__init__.py
@@ -1,11 +1,11 @@
 from .music_element import MusicElement, UrlStreamMusicElement
 from .player import Player
-from .player_manager import MusicAddingResult
-from .player_manager import PlayerManager
+from .player_manager import PlayerManager, MusicAddingResult, MusicRemovingResult
 
 __all__ = [
     'PlayerManager',
     'MusicAddingResult',
+    'MusicRemovingResult',
     'Player',
 
     'MusicElement',
