1b7fb5e2eb1a80f275d9fe192ec933e3e0e620ec
ityeri <ityeri9@gmail.com>
2026-07-28 17:55:26 +0900

    add: OrderManager.next_element field

--- src/piepy/player_manager/order_manager.py
+++ src/piepy/player_manager/order_manager.py
@@ -4,20 +4,21 @@ from typing import Hashable
 
 
 @dataclass(frozen=True)
-class OrderManager[E: Hashable]:
+class OrderManager[T: Hashable]:
     """
     is_loop, is_random_order field cannot be changed without .change_order_mode
     """
 
-    elements: list[E]
-    current_element: E | None
-    used_elements: set[E] | None
+    elements: list[T]
+    current_element: T | None
+    next_element: T | None
+    used_elements: set[T] | None
 
     is_loop: bool
     is_random_order: bool
 
     @staticmethod
-    def create(elements: list[E], current_element: E | None, is_loop: bool, is_random_order: bool) -> OrderManager[E]:
+    def create(elements: list[T], current_element: T | None, next_element: T | None, *, is_loop: bool, is_random_order: bool) -> OrderManager[T]:
         if is_random_order:
             used_elements = set()
         else:
@@ -30,13 +31,14 @@ class OrderManager[E: Hashable]:
         return OrderManager(
             elements=elements,
             current_element=current_element,
+            next_element=next_element,
             used_elements=used_elements,
 
             is_loop=is_loop,
             is_random_order=is_random_order
         )
 
-    def step(self) -> OrderManager[E]:
+    def step(self) -> OrderManager[T]:
         if self.is_random_order:
             available_elements = set(self.elements) - self.used_elements
 
@@ -52,10 +54,10 @@ class OrderManager[E: Hashable]:
                     used_elements = self.used_elements
 
         else:
-            if self.current_element is None:
+            if self.next_element is None:
                 next_index = 0
             else:
-                next_index = self.elements.index(self.current_element) + 1
+                next_index = self.elements.index(self.next_element) + 1
 
             if len(self.elements) <= next_index:
                 if self.is_loop:
@@ -69,14 +71,15 @@ class OrderManager[E: Hashable]:
 
         return OrderManager(
             elements=self.elements,
-            current_element=next_element,
+            current_element=self.next_element,
+            next_element=next_element,
             used_elements=used_elements,
 
             is_loop = self.is_loop,
             is_random_order = self.is_random_order
         )
 
-    def change_order_mode(self, is_loop: bool, is_random_order: bool) -> OrderManager[E]:
+    def change_order_mode(self, is_loop: bool, is_random_order: bool) -> OrderManager[T]:
         if is_random_order:
             if self.is_random_order: # If manager was a random order previously
                 used_elements = self.used_elements
@@ -88,34 +91,37 @@ class OrderManager[E: Hashable]:
         return OrderManager(
             elements=self.elements,
             current_element=self.current_element,
+            next_element=self.next_element,
             used_elements=used_elements,
 
             is_loop=is_loop,
             is_random_order=is_random_order
         )
 
-    def goto(self, element: E) -> OrderManager[E]:
+    def goto(self, element: T) -> OrderManager[T]:
         if element not in self.elements:
             raise ValueError('Given element does not included in elements')
 
         return OrderManager(
             elements=self.elements,
             current_element=element,
+            next_element=self.next_element,
             used_elements=self.used_elements,
 
             is_loop=self.is_loop,
             is_random_order=self.is_random_order
         )
-    def add_last(self, element: E) -> OrderManager[E]:
+    def add_last(self, element: T) -> OrderManager[T]:
         return OrderManager(
             elements=self.elements + [element],
             current_element=self.current_element,
+            next_element=self.next_element,
             used_elements=self.used_elements,
 
             is_loop=self.is_loop,
             is_random_order=self.is_random_order
         )
-    def rm(self, element: E) -> OrderManager[E]:
+    def rm(self, element: T) -> OrderManager[T]:
         if element not in self.elements:
             raise ValueError('Given element does not included in elements')
 
@@ -125,6 +131,7 @@ class OrderManager[E: Hashable]:
         return OrderManager(
             elements=[e for e in self.elements if e != element],
             current_element=self.current_element,
+            next_element=self.next_element,
             used_elements=self.used_elements - {element},
 
             is_loop=self.is_loop,
