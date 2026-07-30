796232de0eb7b8395afec4fc27bf62dd1bbf691e
ityeri <ityeri9@gmail.com>
2026-07-30 18:40:26 +0900

    edit: All player controlling methods in the PlayerManager are replaced to ready_player, get_player_controller method

--- src/piepy/player_manager/player_manager.py
+++ src/piepy/player_manager/player_manager.py
@@ -1,32 +1,8 @@
-import asyncio
-from dataclasses import dataclass
-from enum import Enum, auto
-
 from discord import VoiceChannel
 from discord.ext import commands
 
-from .music_element import MusicElement
-from .player import Player, PlayerStopEvent, PlayerState
-
-
-class _OperationResult(Enum):
-    def __new__(cls, value, is_success: bool):
-        obj = object.__new__(cls)
-        obj._value_ = value
-        return obj
-
-    def __init__(self, value, is_success: bool):
-        self.is_success = is_success
-
-class MusicAddingResult(_OperationResult):
-    CREATED_AND_ADDED = (auto(), True)
-    ADDED = (auto(), True)
-    DUPLICATED = (auto(), False)
-
-class MusicRemovingResult(_OperationResult):
-    PLAYER_NOT_FOUND = (auto(), False)
-    REMOVED = (auto(), True)
-    SKIPPED_AND_REMOVED = (auto(), True)
+from .player import Player, PlayerStopEvent
+from .player_controller import PlayerController
 
 
 class PlayerManager:
@@ -34,104 +10,29 @@ class PlayerManager:
         self.bot: commands.Bot = bot
         self._players: dict[int, Player] = dict() # guild id: Player
 
-    def _get_player(self, guild_id: int) -> Player | None:
-        try:
-            return self._players[guild_id]
-        except KeyError:
-            return None
-
-    def _create_player(self, guild_id: int) -> Player:
-        if guild_id in self._players:
-            raise RuntimeError(f'Player for given guild_id is already exists (id: {guild_id})')
-
-        created_player = Player(guild_id, on_stop=self._on_player_stop)
-        self._players[guild_id] = created_player
-
-        return created_player
-
     async def _on_player_stop(self, player_stop_event: PlayerStopEvent):
         self._players.pop(player_stop_event.player.guild_id)
 
-    def get_player_state(self, guild_id: int) -> PlayerState | None:
-        player = self._get_player(guild_id)
-        if player is not None:
-            return player.to_player_state()
-        else:
-            return None
-
-    async def play_or_add(
+    async def ready_player(
             self,
             guild_id: int,
             voice_channel: VoiceChannel,
-            music_element: MusicElement,
-    ) -> MusicAddingResult:
-        is_created = False
-
-        player = self._get_player(guild_id)
-        if player is None:
-            player = self._create_player(guild_id)
-
-            await player.ready(voice_channel, self.bot.loop, is_loop=False, is_random_order=False)
-
-            is_created = True
-
-        if music_element in player.musics:
-            return MusicAddingResult.DUPLICATED
-
-        player.add_last(music_element)
-
-        if is_created:
-            player.start()
-
-        return MusicAddingResult.CREATED_AND_ADDED if is_created else MusicAddingResult.ADDED
-
-    async def stop(self, guild_id: int) -> bool:
-        player = self._get_player(guild_id)
-        if player is None:
-            return False
-
-        await player.stop()
-        return True
-
-    def get_musics(self, guild_id: int) -> list[MusicElement] | None:
-        player = self._get_player(guild_id)
-        if player is None:
+            *,
+            is_loop: bool = False,
+            is_random_order: bool = False
+    ) -> PlayerController | None:
+        if guild_id in self._players:
             return None
 
-        return list(player.musics)
+        player = Player(guild_id, self._on_player_stop)
+        await player.ready(voice_channel, self.bot.loop, is_loop=is_loop, is_random_order=is_random_order)
 
-    async def rm_music(self, guild_id: int, music_element: MusicElement) -> MusicRemovingResult:
-        player = self._get_player(guild_id)
-        if player is None:
-            return MusicRemovingResult.PLAYER_NOT_FOUND
+        self._players[guild_id] = player
 
-        skipped = False
+        return PlayerController(player)
 
-        if music_element == player.current_music:
-            future = player.subscribe_next_step()
-            player.jump_to(None)
-            await asyncio.wait_for(future, timeout=None)
-            skipped = True
-
-        player.rm(music_element)
-
-        if skipped:
-            return MusicRemovingResult.SKIPPED_AND_REMOVED
+    async def get_player_controller(self, guild_id: int) -> PlayerController | None:
+        if guild_id in self._players:
+            return PlayerController(self._players[guild_id])
         else:
-            return MusicRemovingResult.REMOVED
-
-    def jump_to_music(self, guild_id: int, music_element: MusicElement | None) -> bool:
-        player = self._get_player(guild_id)
-        if player is None:
-            return False
-
-        player.jump_to(music_element)
-        return True
-
-    def change_order_mode(self, guild_id: int, is_loop: bool, is_random_order: bool) -> bool:
-        player = self._get_player(guild_id)
-        if player is None:
-            return False
-
-        player.change_order_mode(is_loop, is_random_order)
-        return True
+            return None
