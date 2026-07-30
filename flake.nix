6375ae5d99e9389be0417113d2fbcb7383ef06a8
ityeri <ityeri9@gmail.com>
2026-07-30 21:03:39 +0900

    edit: flake.nix file has changed to unified shell hook applies to run environment and dev shell samely

--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         runtimeLibs = with pkgs; [ libopus libsodium ];
+        commonShellHook = ''
+            export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+        '';
       in
       {
         devShells.default = pkgs.mkShell {
@@ -22,15 +26,12 @@
             cacert
           ];
 
-          SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibs;
+          shellHook = commonShellHook;
         };
-
         apps.default = {
           type = "app";
           program = "${pkgs.writeShellScript "piepy" ''
-            export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+            ${commonShellHook}
             exec ${pkgs.uv}/bin/uv run piepy "$@"
           ''}";
         };
