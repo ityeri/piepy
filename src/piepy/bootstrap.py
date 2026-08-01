21116f3abd96245ce6fb2e9fbd33e544ceded61b
ityeri <ityeri9@gmail.com>
2026-08-01 15:39:50 +0900

    edit: Bootstrapper class has changed to uses a logger that named as __name__ variable

--- src/piepy/bootstrap.py
+++ src/piepy/bootstrap.py
@@ -6,11 +6,12 @@ from discord.ext import commands
 
 from piepy.root_container import RootContainer
 
+_logger = logging.getLogger(__name__)
+
 
 class Bootstrapper:
     def __init__(self, root_container: RootContainer):
         self.root_container: RootContainer = root_container
-        self.logger: logging.Logger = logging.getLogger(self.__class__.__name__)
         self.bot: commands.Bot = root_container.bot()
 
     def run(self):
@@ -19,37 +20,37 @@ class Bootstrapper:
     async def arun(self):
         print('Bootstrapper: Setup logging...')
         self.setup_logging()
-        self.logger.info('If you can see it, logging setup is complete')
+        _logger.info('If you can see it, logging setup is complete')
 
         self.bot.add_listener(self.on_ready)
 
-        self.logger.info('Loading MusicCommandCog...')
+        _logger.info('Loading MusicCommandCog...')
         cog = self.root_container.music_command_cog()
         await self.bot.add_cog(cog)
-        self.logger.info('MusicCommandCog loading is done')
+        _logger.info('MusicCommandCog loading is done')
 
-        self.logger.info('Starting PlayerGc...')
+        _logger.info('Starting PlayerGc...')
         player_gc = self.root_container.player_gc()
         asyncio.create_task(player_gc.run())
-        self.logger.info('PlayerGc has started')
+        _logger.info('PlayerGc has started')
 
-        self.logger.info('Starting bot...')
+        _logger.info('Starting bot...')
         config = self.root_container.config()
         bot_task = asyncio.create_task(self.bot.start(token=config.bot_token))
-        self.logger.info('Bot has started')
+        _logger.info('Bot has started')
 
-        self.logger.info('Bootstrapping is complete! now you just waiting for actual log with delicious ramyeon')
+        _logger.info('Bootstrapping is complete! now you just waiting for actual log with delicious ramyeon')
 
         await bot_task
 
     async def on_ready(self):
-        self.logger.info('Bot is now ready!')
-        self.logger.info(f'Logged in as {self.bot.user.name}')
-        self.logger.info('Timings Reset')
+        _logger.info('Bot is now ready!')
+        _logger.info(f'Logged in as {self.bot.user.name}')
+        _logger.info('Timings Reset')
 
-        self.logger.info('Command syncing... (This may take a moment!)')
+        _logger.info('Command syncing... (This may take a moment!)')
         await self.bot.tree.sync()
-        self.logger.info('Command syncing has done')
+        _logger.info('Command syncing has done')
 
     def setup_logging(self):
         root_logger = logging.getLogger()
